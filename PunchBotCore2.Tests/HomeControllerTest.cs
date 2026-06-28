d7896f3fe136f59e7b5073fed642b1cbf5bef73c
chi-lambda <15052875+chi-lambda@users.noreply.github.com>
2026-06-28 14:08:10 +0200

    Add test project and first test

---
+++ PunchBotCore2.Tests/HomeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PunchBotCore2.Controllers;
+using PunchBotCore2.Tests.Mocks;
+
+namespace PunchBotCore2.Tests;
+
+[TestClass]
+public sealed class HomeControllerTest
+{
+    private readonly HomeController _controller = new(new TestPunchContextFactory());
+
+    [TestMethod]
+    public async Task Index_WorksWithInitialDatabase()
+    {
+        var result = await _controller.Index();
+        Assert.IsInstanceOfType<ViewResult>(result);
+    }
+}
---
+++ PunchBotCore2.Tests/Mocks/TestPunchContextFactory.cs
@@ -0,0 +1,36 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using PunchBotCore2.Data;
+
+namespace PunchBotCore2.Tests.Mocks;
+
+public class TestPunchContextFactory : IDbContextFactory<PunchContext>
+{
+    private readonly SqliteConnection _connection;
+    private readonly DbContextOptions<PunchContext> _contextOptions;
+
+    public TestPunchContextFactory()
+    {
+        // Create and open a connection. This creates the SQLite in-memory database, which will persist until the connection is closed
+        // at the end of the test (see Dispose below).
+        _connection = new SqliteConnection("Filename=:memory:");
+        _connection.Open();
+
+        // These options will be used by the context instances in this test suite, including the connection opened above.
+        _contextOptions = new DbContextOptionsBuilder<PunchContext>()
+            .UseSqlite(_connection)
+            .Options;
+        using PunchContext context = new(_contextOptions);
+        context.Database.EnsureCreated();
+    }
+    
+    public PunchContext CreateDbContext()
+    {
+        return new PunchContext(_contextOptions);
+    }
+
+    public void Dispose()
+    {
+        _connection.Dispose();
+    }
+}
