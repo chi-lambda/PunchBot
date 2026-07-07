683ffbfd5e8046e473b68ea9234650e19120e780
chi-lambda <15052875+chi-lambda@users.noreply.github.com>
2026-07-07 15:46:13 +0200

    exclude DateTimeService from coverage

--- PunchBotCore2/Util/DateTimeService.cs
+++ PunchBotCore2/Util/DateTimeService.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace PunchBotCore2.Util;
 
+[ExcludeFromCodeCoverage]
 public class DateTimeService : IDateTimeService
 {
     public DateTime Now => DateTime.Now;
